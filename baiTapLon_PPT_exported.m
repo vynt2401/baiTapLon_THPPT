ff8c5cc97aa142312b0d48a4806357778e52f45e
vynt2401 <nvy381932@gmail.com>
2024-12-18 17:28:08 +0700

    them cac thuoc tinh nut nhan o tab dao ham

--- baiTapLon_PPT_exported.m
+++ baiTapLon_PPT_exported.m
@@ -2,15 +2,34 @@ classdef baiTapLon_PPT_exported < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure        matlab.ui.Figure
-        TabGroup        matlab.ui.container.TabGroup
-        Tim_Nghiem_Tab  matlab.ui.container.Tab
-        Noi_Suy_Tab     matlab.ui.container.Tab
-        Hoi_Quy_Tab     matlab.ui.container.Tab
-        Dao_Ham_Tab     matlab.ui.container.Tab
-        Tich_Phan_Tab   matlab.ui.container.Tab
-        Introduce_Tab   matlab.ui.container.Tab
+        UIFigure                   matlab.ui.Figure
+        TabGroup                   matlab.ui.container.TabGroup
+        Tim_Nghiem_Tab             matlab.ui.container.Tab
+        NhpphngtrnhEditFieldLabel  matlab.ui.control.Label
+        inputEquation              matlab.ui.control.EditField
+        NhpkhongphnlynghimEditFieldLabel  matlab.ui.control.Label
+        inputSeparationDistance    matlab.ui.control.EditField
+        NhpsaischophpLabel         matlab.ui.control.Label
+        inputAllowableError        matlab.ui.control.EditField
+        ChnphngphptmLabel          matlab.ui.control.Label
+        choseOption                matlab.ui.control.DropDown
+        KtqunghimEditFieldLabel    matlab.ui.control.Label
+        result                     matlab.ui.control.NumericEditField
+        SlnlpEditFieldLabel        matlab.ui.control.Label
+        result_Iterations          matlab.ui.control.NumericEditField
+        UIAxes                     matlab.ui.control.UIAxes
+        Noi_Suy_Tab                matlab.ui.container.Tab
+        Hoi_Quy_Tab                matlab.ui.container.Tab
+        Dao_Ham_Tab                matlab.ui.container.Tab
+        Tich_Phan_Tab              matlab.ui.container.Tab
+        Introduce_Tab              matlab.ui.container.Tab
+        EditFieldLabel             matlab.ui.control.Label
+        Test_Field                 matlab.ui.control.EditField
     end
+
+    
+    %this line trust comment in to test
+    
 
     % Component initialization
     methods (Access = private)
@@ -28,53 +47,115 @@ classdef baiTapLon_PPT_exported < matlab.apps.AppBase
             app.TabGroup.Position = [1 0 850 532];
 
             % Create Tim_Nghiem_Tab
-            
-            %Vỹ sẽ làm ở tab này
-            
             app.Tim_Nghiem_Tab = uitab(app.TabGroup);
             app.Tim_Nghiem_Tab.Title = 'Nghiệm';
 
+            % Create NhpphngtrnhEditFieldLabel
+            app.NhpphngtrnhEditFieldLabel = uilabel(app.Tim_Nghiem_Tab);
+            app.NhpphngtrnhEditFieldLabel.HorizontalAlignment = 'right';
+            app.NhpphngtrnhEditFieldLabel.Position = [47 441 107 22];
+            app.NhpphngtrnhEditFieldLabel.Text = 'Nhập phương trình';
+
+            % Create inputEquation
+            app.inputEquation = uieditfield(app.Tim_Nghiem_Tab, 'text');
+            app.inputEquation.Position = [177 441 113 22];
+
+            % Create NhpkhongphnlynghimEditFieldLabel
+            app.NhpkhongphnlynghimEditFieldLabel = uilabel(app.Tim_Nghiem_Tab);
+            app.NhpkhongphnlynghimEditFieldLabel.HorizontalAlignment = 'right';
+            app.NhpkhongphnlynghimEditFieldLabel.Position = [0 407 162 22];
+            app.NhpkhongphnlynghimEditFieldLabel.Text = 'Nhập khoảng phân ly nghiệm';
+
+            % Create inputSeparationDistance
+            app.inputSeparationDistance = uieditfield(app.Tim_Nghiem_Tab, 'text');
+            app.inputSeparationDistance.Position = [177 407 113 22];
+
+            % Create NhpsaischophpLabel
+            app.NhpsaischophpLabel = uilabel(app.Tim_Nghiem_Tab);
+            app.NhpsaischophpLabel.HorizontalAlignment = 'right';
+            app.NhpsaischophpLabel.Position = [38 362 122 22];
+            app.NhpsaischophpLabel.Text = 'Nhập sai số cho phép';
+
+            % Create inputAllowableError
+            app.inputAllowableError = uieditfield(app.Tim_Nghiem_Tab, 'text');
+            app.inputAllowableError.Position = [177 362 113 22];
+
+            % Create ChnphngphptmLabel
+            app.ChnphngphptmLabel = uilabel(app.Tim_Nghiem_Tab);
+            app.ChnphngphptmLabel.HorizontalAlignment = 'right';
+            app.ChnphngphptmLabel.Position = [38 318 130 22];
+            app.ChnphngphptmLabel.Text = 'Chọn phương pháp tìm';
+
+            % Create choseOption
+            app.choseOption = uidropdown(app.Tim_Nghiem_Tab);
+            app.choseOption.Items = {'Chia đôi', 'Lặp', 'Newton(Tiếp tuyến)'};
+            app.choseOption.Position = [177 318 113 22];
+            app.choseOption.Value = 'Chia đôi';
+
+            % Create KtqunghimEditFieldLabel
+            app.KtqunghimEditFieldLabel = uilabel(app.Tim_Nghiem_Tab);
+            app.KtqunghimEditFieldLabel.HorizontalAlignment = 'right';
+            app.KtqunghimEditFieldLabel.Position = [102 103 90 22];
+            app.KtqunghimEditFieldLabel.Text = 'Kết quả nghiệm';
+
+            % Create result
+            app.result = uieditfield(app.Tim_Nghiem_Tab, 'numeric');
+            app.result.Position = [207 103 100 22];
+
+            % Create SlnlpEditFieldLabel
+            app.SlnlpEditFieldLabel = uilabel(app.Tim_Nghiem_Tab);
+            app.SlnlpEditFieldLabel.HorizontalAlignment = 'right';
+            app.SlnlpEditFieldLabel.Position = [636 103 59 22];
+            app.SlnlpEditFieldLabel.Text = 'Số lần lặp';
+
+            % Create result_Iterations
+            app.result_Iterations = uieditfield(app.Tim_Nghiem_Tab, 'numeric');
+            app.result_Iterations.Position = [710 103 100 22];
+
+            % Create UIAxes
+            app.UIAxes = uiaxes(app.Tim_Nghiem_Tab);
+            title(app.UIAxes, 'Đồ thị hàm số  của phương trình')
+            xlabel(app.UIAxes, 'X')
+            ylabel(app.UIAxes, 'Y')
+            zlabel(app.UIAxes, 'Z')
+            app.UIAxes.Position = [321 167 528 308];
+
             % Create Noi_Suy_Tab
-            
-            %Vỹ sẽ làm ở tab này
-            
             app.Noi_Suy_Tab = uitab(app.TabGroup);
             app.Noi_Suy_Tab.Title = 'Nội Suy';
 
             % Create Hoi_Quy_Tab
-            
-            %Tiến sẽ hồi quy chuyển kiếp ở tab này
-            
             app.Hoi_Quy_Tab = uitab(app.TabGroup);
             app.Hoi_Quy_Tab.Title = 'Hồi Quy';
 
             % Create Dao_Ham_Tab
-            
-            %Thành sẽ làm ở Tab Đạo hàm
-            
             app.Dao_Ham_Tab = uitab(app.TabGroup);
             app.Dao_Ham_Tab.Title = 'Đạo Hàm';
 
             % Create Tich_Phan_Tab
-            
-            %Thuận sẽ làm ở tab Tích Phân
-            
             app.Tich_Phan_Tab = uitab(app.TabGroup);
             app.Tich_Phan_Tab.Title = 'Tích Phân';
 
             % Create Introduce_Tab
-            
-            %Nhường Tâm slot cuối
-            
             app.Introduce_Tab = uitab(app.TabGroup);
             app.Introduce_Tab.Title = 'Giới Thiệu Nhóm';
+
+            % Create EditFieldLabel
+            app.EditFieldLabel = uilabel(app.Introduce_Tab);
+            app.EditFieldLabel.HorizontalAlignment = 'right';
+            app.EditFieldLabel.Position = [88 453 55 22];
+            app.EditFieldLabel.Text = 'Edit Field';
+
+            % Create Test_Field
+            app.Test_Field = uieditfield(app.Introduce_Tab, 'text');
+            app.Test_Field.Position = [158 453 164 22];
+            app.Test_Field.Value = 'Tab này cho Tâm làm';
 
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
         end
     end
 
-      
     % App creation and deletion
     methods (Access = public)
 
@@ -100,5 +181,3 @@ classdef baiTapLon_PPT_exported < matlab.apps.AppBase
         end
     end
 end
-
-%created by Nguyen The Vy
